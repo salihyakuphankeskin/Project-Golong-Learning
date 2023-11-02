160d199fefe73a66372f1208c3bb12539b47edde
Cadostropia <salihyakuphankeskin@gmail.com>
2023-11-02 18:14:04 +0300

    0.0.1 starting up project

---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+
+func main(){
+	var name string = "Jeff"
+	surname, age := "Carnass", 25
+	fmt.Println(name, surname, age)
+	fmt.Println("HI THERE")
+	fmt.Println("HI bro")
+	fmt.Scanln()
+}
