0f7398eed5b1215152a768fa84217162ce7b9719
salihyakuphankeskin <salihyakuphankeskin@gmail.com>
2023-11-04 23:48:37 +0300

    introduction to functions

--- hello.go
+++ hello.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"math/rand"
 )
 
@@ -9,10 +9,69 @@ import (
 func main(){
 	var name string = "Jeff"
 	surname, age := "Carnass", 25
+	const basicPlanName = "Basic Plan"
 
 	fmt.Println(name, surname, age)
 	fmt.Println("HI THERE")
 	fmt.Println("HI bro")
 	fmt.Println("My favorite number is", rand.Intn(10))
+	
 	fmt.Scanln()
+}
+func typeFinder(value any){
+	fmt.Printf("The type of penniesPerText is %T\n", value)
+}
+func twoValueSameLine(){
+	monthName, nextMonthName := "July", "August"
+	fmt.Println(monthName, nextMonthName)
+}
+func floatToInt(value float64) int{
+
+	valueInt := int(value)
+	return valueInt
+}
+func secondsInHour(){
+	const secondsInMinute = 60
+	const minutesInHour = 60
+	const secondsInHour = secondsInMinute * minutesInHour
+
+	fmt.Println("number of seconds in an hour:", secondsInHour)
+}
+
+func printFunctions(){
+	var myString string
+	//	Interpolate the default representation
+	myString = fmt.Sprintf("I am %v years old", 10)
+	println(myString)	// I am 10 years old
+
+	myString = fmt.Sprintf("I am %v years old", "way too many")
+	println(myString)	// I am way too many years old
+
+	myString = fmt.Sprintf("I am %s years old", "way too many")
+	println(myString)	// I am way too many years old
+
+	myString = fmt.Sprintf("I am %d years old", 10)
+	println(myString)	// I am 10 years old
+
+	myString = fmt.Sprintf("I am %f years old", 10.523)
+	println(myString)	// I am 10.523000 years old
+
+	myString = fmt.Sprintf("I am %.2f years old", 10.523)
+	println(myString)	// I am 10.52 years old
+
+	const name = "Saul Goodman"
+	const openRate = 30.5
+
+	msg := fmt.Sprintf("Hi %v, your open rate is %v percent" ,name , openRate)
+	fmt.Println(msg)
+}
+func ifStatements(){
+	if 1>0{
+		fmt.Println("one is bigger than zero")
+	}
+	appleColor:="Red"
+
+	if currentColor:="Green";currentColor == appleColor{
+		fmt.Println("current color and apple color is matched")
+	}
 }
