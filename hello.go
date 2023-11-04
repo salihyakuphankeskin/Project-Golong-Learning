112474173f61eb3e240170ad9b26134214928cfa
salihyakuphankeskin <salihyakuphankeskin@gmail.com>
2023-11-04 17:15:08 +0300

    using math

--- hello.go
+++ hello.go
@@ -2,14 +2,17 @@ package main
 
 import (
     "fmt"
+	"math/rand"
 )
 
 
 func main(){
 	var name string = "Jeff"
 	surname, age := "Carnass", 25
+
 	fmt.Println(name, surname, age)
 	fmt.Println("HI THERE")
 	fmt.Println("HI bro")
+	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Scanln()
 }
